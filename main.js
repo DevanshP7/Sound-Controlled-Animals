692d6472aefc7da31b67651fb282cdae8e09f141
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2022-01-05 23:18:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
 function startClassification(){
 
-    Navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.audioClassify('https://teachablemachine.withgoogle.com/models/--Xx2NNXC/model.json', model_read);
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/--Xx2NNXC/model.json', modelread);
 
 }
 
-function model_read(){
+function modelread(){
 
     console.log('Model ready');
-    classifier.Classify(got_result);
+    classifier.classify(got_result);
 
+}
+
+dog = 0;
+cat = 0;
+
+function got_result(error, results){
+
+    if(error){
+
+        console.error(error);
+
+    }else{
+
+        console.log(results);
+
+        red = Math.floor(Math.random()*255)+1;
+        green = Math.floor(Math.random()*255)+1;
+        blue = Math.floor(Math.random()*255)+1;
+
+        document.getElementById("accuracy").innerHTML = 'Detected Dog: ' + dog + ', Detected Cat: ' + cat;
+        document.getElementById("sound").innerHTML = results[0].label;
+        document.getElementById("accuracy").style.color = 'rgba('+red+','+green+','+blue+');';
+        document.getElementById("sound").style.color = 'rgba('+red+','+green+','+blue+');';
+
+        gif_tag = document.getElementById("gif");
+
+        if(results[0].label == 'Bark'){
+
+            gif_tag.src = "i_barking.gif";
+            dog = dog + 1;
+
+        }else if(results[0].label == 'Meow'){
+
+            gif_tag.src = "i_meowing.gif";
+            cat = cat + 1;
+
+        }else{
+
+            gif_tag.src = "i_listening.gif";
+
+        }
+
+    }
+    
 }
